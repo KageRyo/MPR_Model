1fdbe56cadbb2a5949e712b3e56d03ee3aa10ab5
影凌 <baby980103@gmail.com>
2024-03-24 16:43:07 +0800

    feat: update model to v6.01 pki.

--- API.py
+++ API.py
@@ -1,16 +1,15 @@
+import pandas as pd
 import joblib
 from fastapi import FastAPI
 from typing import Optional
-import pandas as pd
-app = FastAPI()
-
 from enum import Enum
 
+app = FastAPI()
 
 @app.get("/blog/pagination/{csv}")
 def predict(csv:str):
     data=pd.read_csv(f'{csv}')
     df = pd.DataFrame(data)
-    model=joblib.load('modelEXVer.4.01.pkl')
+    model=joblib.load('modelEXVer.6.01.pkl')
     predictions = model.predict(df)
     return {"message": f"預測結果：{predictions}"}
